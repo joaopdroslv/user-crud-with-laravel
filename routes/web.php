ad73fda3a5f75d01c9231ea7a7abf16ec3833f43
João Pedro <pdrojoao.slv@gmail.com>
2024-10-28 01:51:15 -0300

    Homepage route removed

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/', [UserController::class, 'index']);
 
 /* 
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
